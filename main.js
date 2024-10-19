4ecceccd9f815adef1bb45c6606ccabe7d77d6af
Ersultan Mansur <nullmansurov@gmail.com>
2024-10-19 10:40:11 +0500

    Update main.js

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ let mainWindow;
 
 function createWindow() {
     mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1200,
+        height: 900,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'), // Убедитесь, что этот файл существует
             contextIsolation: true, // Включите контекстную изоляцию для безопасности
